babda24dc8979bdd4fdd34dea3e72c569afe3946
Pavel <pavelparshin2002005@gmail.com>
2024-12-25 17:18:41 +0300

    feat: Create post component

--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -1,28 +1,5 @@
-import { useEffect, useState } from "react";
-import { fetchPosts } from "../../api/posts";
-
 const HomePage = () => {
-  const [posts, setPosts] = useState<any[]>([]);
-
-  useEffect(() => {
-    const loadPosts = async () => {
-      const fetchedPosts = await fetchPosts();
-      setPosts(fetchedPosts);
-    };
-    loadPosts();
-  }, []);
-
-  return (
-    <div>
-      <h1>Home Page</h1>
-      {posts.map((post) => (
-        <div key={post.id} className="post-card">
-          <h2>{post.title}</h2>
-          <p>{post.body.slice(0, 100)}...</p>
-        </div>
-      ))}
-    </div>
-  );
+  return <div></div>;
 };
 
 export default HomePage;
