0ef46d2cbb8ca15ac5a80d0c1c4289e54ad5b8a0
Pavel <pavelparshin2002005@gmail.com>
2024-12-25 19:25:58 +0300

    feat: add pagination logic

--- src/api/posts.ts
+++ src/api/posts.ts
@@ -2,9 +2,11 @@ import axios from "axios";
 
 const URL = "https://jsonplaceholder.typicode.com/posts";
 
-export const fetchPosts = async () => {
+export const fetchPosts = async (page: number, limit: number) => {
   try {
-    const response = await axios.get(URL);
+    const response = await axios.get(URL, {
+      params: { _page: page, _limit: limit },
+    });
     return response.data;
   } catch (error) {
     console.error("Error fetching posts:", error);
