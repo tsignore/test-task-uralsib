5f866e78f4f5c406a173a01ed8a4b042c88f7649
Pavel <pavelparshin2002005@gmail.com>
2024-12-27 11:51:54 +0300

    chore: remove extra code

--- src/features/infiniteScroll/useInfiniteScroll.ts
+++ src/features/infiniteScroll/useInfiniteScroll.ts
@@ -1,21 +1,19 @@
-// features/infiniteScroll/useInfiniteScroll.ts
-
 import { useEffect } from "react";
 
 export const useInfiniteScroll = (
-  loading: boolean, // Указывает, идет ли сейчас загрузка
-  hasMore: boolean, // Есть ли еще посты для загрузки
-  loadMore: () => void // Функция для подгрузки данных
+  loading: boolean,
+  hasMore: boolean,
+  loadMore: () => void
 ) => {
   const handleScroll = () => {
-    if (loading || !hasMore) return; // Если идет загрузка или больше нет данных, не обрабатываем скролл
+    if (loading || !hasMore) return;
 
     const bottom =
       window.innerHeight + document.documentElement.scrollTop ===
       document.documentElement.offsetHeight;
 
     if (bottom) {
-      loadMore(); // Если мы на дне страницы, загружаем больше данных
+      loadMore();
     }
   };
 
@@ -25,5 +23,5 @@ export const useInfiniteScroll = (
     return () => {
       window.removeEventListener("scroll", handleScroll);
     };
-  }, [loading, hasMore]); // Обновление события, если загрузка или состояние данных изменяются
+  }, [loading, hasMore]);
 };
